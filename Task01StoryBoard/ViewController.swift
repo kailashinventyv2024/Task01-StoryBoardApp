99727bdf5931732ca74f946d636339979ff74b77
kailashinventyv2024 <kailash.rajput.inventyv@gmail.com>
2025-02-10 16:56:52 +0530

    basic storyboard iOS app

---
+++ Task01StoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task01StoryBoard
+//
+//  Created by Kailash Rajput on 10/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
